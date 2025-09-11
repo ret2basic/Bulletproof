8e94aafed3e2e9a9512d618d2f0d0538fdbcd5a7
ret2basic.eth <ret2basic@zksecurity.xyz>
2025-09-11 22:01:25 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Bulletproof
-RareSkills Bulletproof implementation
+# ZK-bulletproofs
+Exercises for Bulletproofs section of the [ZK Book](https://www.rareskills.io/zk-book)
+
+You will need to install the following Python libraries using your favorite package manager:
+
+* https://pypi.org/project/libnum/
+* https://github.com/ethereum/py_ecc
+* https://numpy.org/install/
---
+++ chapter-08.py
@@ -0,0 +1,14 @@
+
+# Test cases
+a1 = [808, 140, 166, 209]
+b1 = [88, 242, 404, 602]
+
+a2 = [433, 651]
+b2 = [282, 521]
+
+a3 = [222]
+a4 = [313]
+
+# Prove you know the inner product of a and b using a combination of the algorithm from Chapter 5 and Chapter 7.
+# Your interactive proof should transmit no more than logarithmic size data and be zero knowledge even if n = 1.
+# Use a random elliptic curve basis based on the algorithm from Chapter 1.
