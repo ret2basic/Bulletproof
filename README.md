b2ebfb09e4c55f1bcf2d49fb8e9cc2c896c376dc
ret2basic.eth <ret2basic@zksecurity.xyz>
2025-09-13 13:29:41 +0800

    chapter 2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # ZK-bulletproofs
+
 Exercises for Bulletproofs section of the [ZK Book](https://www.rareskills.io/zk-book)
 
 You will need to install the following Python libraries using your favorite package manager:
@@ -6,3 +7,15 @@ You will need to install the following Python libraries using your favorite pack
 * https://pypi.org/project/libnum/
 * https://github.com/ethereum/py_ecc
 * https://numpy.org/install/
+
+## Progress
+
+- [x] chapter-01.py
+- [ ] chapter-02.py
+- [ ] chapter-03.py
+- [ ] chapter-04.py
+- [ ] chapter-05.py
+- [ ] chapter-06.py
+- [ ] chapter-07a.py
+- [ ] chapter-07b.py
+- [ ] chapter-08.py
--- chapter-02.py
+++ chapter-02.py
@@ -1,29 +1,36 @@
-from py_ecc.bn128 import is_on_curve, FQ
+from py_ecc.bn128 import is_on_curve, FQ, G1
 from py_ecc.fields import field_properties
 field_mod = field_properties["bn128"]["field_modulus"]
 from hashlib import sha256
 from libnum import has_sqrtmod_prime_power, sqrtmod_prime_power
 
 b = 3 # for bn128, y^2 = x^3 + 3
-seed = "RareSkills"
+public_random = "RareSkills"  # publicly agreed random string / number
 
-x = int(sha256(seed.encode('ascii')).hexdigest(), 16) % field_mod 
-
-entropy = 0
+# initial x: hash(generator_x || generator_y || public_random) per algorithm description
+g_x = G1[0].n  # underlying int inside FQ
+g_y = G1[1].n
+x = int(sha256(f"{g_x}{g_y}{public_random}".encode('ascii')).hexdigest(), 16) % field_mod
 
 vector_basis = []
-# modify the code below to generate n points
-while not has_sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1):
-    # increment x, so hopefully we are on the curve
-    x = (x + 1) % field_mod
-    entropy = entropy + 1
 
-# pick the upper or lower point depending on if entropy is even or odd
-y = list(sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1))[entropy & 1 == 0]
-point = (FQ(x), FQ(y))
-assert is_on_curve(point, b), "sanity check"
-vector_basis.append(point)
+# number of points desired
+n = 4
 
-# new x value
-x = int(sha256(str(x).encode('ascii')).hexdigest(), 16) % field_mod 
+for _ in range(n):
+    entropy = 0
+    # find an x that is on the curve (so that x^3 + b is a quadratic residue)
+    while not has_sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1):
+        x = (x + 1) % field_mod
+        entropy += 1
+
+    # pick the upper or lower y based on parity of entropy (deterministic choice)
+    y = list(sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1))[entropy & 1 == 0]
+    point = (FQ(x), FQ(y))
+    assert is_on_curve(point, b), "sanity check"
+    vector_basis.append(point)
+
+    # derive next starting x: hash(x || y || public_random)
+    x = int(sha256(f"{point[0].n}{point[1].n}{public_random}".encode('ascii')).hexdigest(), 16) % field_mod 
+
 print(vector_basis)
