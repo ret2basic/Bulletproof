52b8906f073f434c1d43fb8f8f823d5a9281a5cf
ret2basic.eth <ret2basic@zksecurity.xyz>
2025-09-14 21:51:47 +0800

    chapter 3

--- chapter-03.py
+++ chapter-03.py
@@ -3,7 +3,7 @@ from py_ecc.bn128 import curve_order as p
 import random
 
 def random_field_element():
-    return random.randint(0, curve_order)
+    return random.randint(0, p - 1)
 
 # these EC points have unknown discrete logs:
 G = (FQ(6286155310766333871795042970372566906087502116590250812133967451320632869759), FQ(2167390362195738854837661032213065766665495464946848931705307210578191331138))
@@ -18,7 +18,11 @@ B = (FQ(128486065350455871287888893172307515183924786911123755697753900951123306
 def commit(f_0, f_1, f_2, gamma_0, gamma_1, gamma_2, G, B):
     # fill this in
     # return the commitments as a tuple (C0, C1, C2)
-    pass
+    return (
+        add(multiply(G, f_0), multiply(B, gamma_0)),
+        add(multiply(G, f_1), multiply(B, gamma_1)),
+        add(multiply(G, f_2), multiply(B, gamma_2)),
+    )
 
 def evaluate(f_0, f_1, f_2, u):
     return (f_0 + f_1 * u + f_2 * u**2) % p
@@ -26,29 +30,29 @@ def evaluate(f_0, f_1, f_2, u):
 def prove(gamma_0, gamma_1, gamma_2, u):
     # fill this in
     # return pi
-    pass
+    return (gamma_0 + gamma_1 * u + gamma_2 * u**2) % p
 
-def verify(C0, C1, C2, G, B, f_u, pi):
+def verify(C0, C1, C2, G, B, f_u, pi, u):
     # fill this in
     # Return true or false
-    pass
+    return add(add(C0, multiply(C1, u)), multiply(C2, u**2)) == add(multiply(G, f_u), multiply(B, pi))
 
 ## step 0: Prover and verifier agree on G and B
 
 ## step 1: Prover creates the commitments
 ### f(x) = f_0 + f_1x + f_2x^2
-f_0 = ...
-f_1 = ...
-f_2 = ...
+f_0 = 666
+f_1 = 1337
+f_2 = 42
 
 ### blinding terms
-gamma_0 = ...
-gamma_1 = ...
-gamma_2 = ...
+gamma_0 = 111
+gamma_1 = 222
+gamma_2 = 333
 C0, C1, C2 = commit(f_0, f_1, f_2, gamma_0, gamma_1, gamma_2, G, B)
 
 ## step 2: Verifier picks u
-u = ...
+u = 64
 
 ## step 3: Prover evaluates f(u) and pi
 
@@ -56,7 +60,7 @@ f_u = evaluate(f_0, f_1, f_2, u)
 pi = prove(gamma_0, gamma_1, gamma_2, u)
 
 ## step 4: Verifier accepts or rejects
-if verify(C0, C1, C2, G, B, f_u, pi):
+if verify(C0, C1, C2, G, B, f_u, pi, u):
     print("accept")
 else:
     print("reject")
