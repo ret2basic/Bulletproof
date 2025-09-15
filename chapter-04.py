77b8c0025e237f1b5a93e00243be85060d6938fe
ret2basic.eth <ret2basic@zksecurity.xyz>
2025-09-15 14:40:04 +0800

    chapter 4

--- chapter-04.py
+++ chapter-04.py
@@ -20,9 +20,13 @@ def addd(A, B, C):
 # EC addition example: add(multiply(G, 42), multiply(G, 100))
 
 # remember to do all arithmetic modulo p
-def commit(a, sL, b, sR, alpha, beta, gamma, tau_1, tau_2):
-    pass
-    # return (A, S, V, T1, T2)
+def commit(a, sL, b, sR, alpha, beta, tau_0, tau_1, tau_2):
+    A = add(add(multiply(G, a), multiply(H, b)), multiply(B, alpha))
+    S = add(add(multiply(G, sL), multiply(H, sR)), multiply(B, beta))
+    T0 = add(multiply(G, a * b % p), multiply(B, tau_0))
+    T1 = add(multiply(G, a * sR % p + b * sL % p), multiply(B, tau_1))
+    T2 = add(multiply(G, sL * sR % p), multiply(B, tau_2))
+    return (A, S, T0, T1, T2)
 
 
 def evaluate(f_0, f_1, f_2, u):
@@ -31,29 +35,29 @@ def evaluate(f_0, f_1, f_2, u):
 def prove(blinding_0, blinding_1, blinding_2, u):
     # fill this in
     # return pi
-    pass
+    return (blinding_0 + blinding_1 * u + blinding_2 * (u**2)) % p
 
 ## step 0: Prover and verifier agree on G and B
 
 ## step 1: Prover creates the commitments
-a = ...
-b = ...
-sL = ...
-sR = ...
-t1 = ...
-t2 = ...
+a = 123
+b = 456
+sL = 1234
+sR = 1337
+t1 = (a * sR + b * sL) % p
+t2 = (sL * sR) % p
 
 ### blinding terms
-alpha = ...
-beta = ...
-gamma = ...
-tau_1 = ...
-tau_2 = ...
+alpha = 111
+beta = 222
+tau_0 = 333
+tau_1 = 444
+tau_2 = 555
 
-A, S, V, T1, T2 = commit(a, sL, b, sR, alpha, beta, gamma, tau_1, tau_2)
+A, S, T0, T1, T2 = commit(a, sL, b, sR, alpha, beta, tau_0, tau_1, tau_2)
 
 ## step 2: Verifier picks u
-u = ...
+u = 1314521
 
 ## step 3: Prover evaluates l(u), r(u), t(u) and creates evaluation proofs
 l_u = evaluate(a, sL, 0, u)
@@ -61,9 +65,10 @@ r_u = evaluate(b, sR, 0, u)
 t_u = evaluate(a*b, t1, t2, u)
 
 pi_lr = prove(alpha, beta, 0, u)
-pi_t = prove(gamma, tau_1, tau_2, u)
+pi_t = prove(tau_0, tau_1, tau_2, u)
 
 ## step 4: Verifier accepts or rejects
 assert t_u == (l_u * r_u) % p, "tu != lu*ru"
 assert eq(add(A, multiply(S, u)), addd(multiply(G, l_u), multiply(H, r_u), multiply(B, pi_lr))), "l_u or r_u not evaluated correctly"
-assert eq(add(multiply(G, t_u), multiply(B, pi_t)), addd(V, multiply(T1, u), multiply(T2, u**2 % p))), "t_u not evaluated correctly"
+assert eq(add(multiply(G, t_u), multiply(B, pi_t)), addd(T0, multiply(T1, u), multiply(T2, u**2 % p))), "t_u not evaluated correctly"
+print("accept")
